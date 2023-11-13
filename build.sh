9873df46d257081ee112d5ff1dc23377e63a2e93
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-11-12 20:15:42 -0800

    Drop BootStrap
    This project is mostly out of scope of nds-hb-menu. It is just a menu.
    There are other projects out there that will let direct-booting to
    hbmenu happen.

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 mkdir -p data
 make -C bootloader LOADBIN=$PWD/data/load.bin
-make -C BootStrap
 make -C bootstub
 make -C nds-exception-stub STUBBIN=$PWD/data/exceptionstub.bin
 make
--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 make -C bootloader clean
 make -C bootstub clean
 make -C nds-exception-stub clean
-make -C BootStrap clean
 make clean
 rm -rf data
